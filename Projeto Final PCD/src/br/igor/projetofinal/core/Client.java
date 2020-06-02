410c5c55e208d9032049019b4e1967f7672aa433
Igor de Castro <igor.cbs@gmail.com>
2020-06-01 21:25:04 -0300

    adicionando logica de comunicacao entre Cliente e Server

--- Projeto Final PCD/src/br/igor/projetofinal/core/Client.java
+++ Projeto Final PCD/src/br/igor/projetofinal/core/Client.java
@@ -1,11 +1,42 @@
 package br.igor.projetofinal.core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class Client {
+public class Client extends Thread{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Cliente iniciando!");
+		
+		//Criando socket de comunicação
+		Socket soc =  new Socket("localhost",9010);
+		
+		//Ler o Buffer do console!!!
+		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+		
+		//Ler o Buffer do Socket em si!
+		BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+		
+		//Fazendo o envio de dados para o outputStream, ou seja, via Socket
+		PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+		
+		//Logica de front-end do projeto
+		
+		
+		
+		
+		//Finalizando socket, bufferes e printWriter
+		soc.close();
+		userInput.close();
+		in.close();
+		out.close();
+		
 	}
 
 }
--- Projeto Final PCD/src/br/igor/projetofinal/core/Server.java
+++ Projeto Final PCD/src/br/igor/projetofinal/core/Server.java
@@ -1,10 +1,49 @@
 package br.igor.projetofinal.core;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println("Aguardando conexào com o cliente...");
+		
+		//Criando nova conexão entre cliente e servidor, sem estabelecer comunicação
+		ServerSocket ss = new ServerSocket(9010);
+		
+		//Permitindo conexão com novos cliente
+		Socket soc = ss.accept();
+		
+		System.out.println("Estabelecendo conexão com cliente: " + soc);
+		
+		//Receptor de informações, guardamos ela em um buffer, os dados que viram do Socket!
+		BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+		
+		//Criou o buffer de envio de dados!
+		//Esse construtor com autoFlush significa que nao vou ficar guardando dados no buffer
+		//aguardando entao ser enviado! Ele recebe os dados e da flush(envia pro cliente) na mesma hora
+		//Sem ele, precisamos chamar o metodo flush() sempre que quisermos estabelecer esse envio de dados
+		PrintWriter out = new PrintWriter(soc.getOutputStream(),true);
+		
+		//Logica de atribuição do projeto
+		
+		
+		
+		
+		
+		
+		
+		//Finalizando comunicação com o Cliente fechando Server, Socket, Buffer e Print
+		ss.close();
+		soc.close();
+		in.close();
+		out.close();
 	}
 
 }
